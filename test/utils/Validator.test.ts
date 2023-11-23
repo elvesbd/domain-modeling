85d83ef20aadbc9592ae08837b82313b077838e7
elvesbd <elvesbd@yahoo.com>
2023-11-23 19:58:58 -0300

    test: ensures correct functioning of the non-empty validator

--- test/utils/Validator.test.ts
+++ test/utils/Validator.test.ts
@@ -1,16 +1,36 @@
 import Validator from '@/core/utils/Validator';
 
 describe('Validator', () => {
-  const errorMessage = 'Invalid Text!';
-
   it('should be return null if text not null', () => {
-    const error = Validator.isNotNull('Good morning', errorMessage);
+    const error = Validator.isNotNull('Good morning', 'error');
     expect(error).toBeNull();
   });
 
   it('should be return error if text is null', () => {
-    const error = Validator.isNotNull(null, errorMessage);
+    const error = Validator.isNotNull(null, 'error');
     expect(error).not.toBeNull();
-    expect(error).toBe(errorMessage);
+    expect(error).toBe('error');
+  });
+
+  it('should be return null if text is not empty', () => {
+    const error = Validator.isNotEmpty('Good morning', 'error');
+    expect(error).toBeNull();
+  });
+
+  it('should be return error if text is empty', () => {
+    const error = Validator.isNotEmpty('     ', 'error');
+    expect(error).not.toBeNull();
+    expect(error).toBe('error');
+  });
+
+  it('should be return null if text less than max length', () => {
+    const error = Validator.isLessThan('test', 6, 'error');
+    expect(error).toBeNull();
+  });
+
+  it('should be return error if text more than max length', () => {
+    const error = Validator.isLessThan('test error', 6, 'error');
+    expect(error).not.toBeNull();
+    expect(error).toBe('error');
   });
 });
