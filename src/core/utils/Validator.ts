941e4165532fa37fe1ee779487e32baf680be817
elvesbd <elvesbd@yahoo.com>
2023-11-23 19:27:36 -0300

    feat: add validation for not null

--- src/core/utils/Validator.ts
+++ src/core/utils/Validator.ts
@@ -1,4 +1,8 @@
 export default class Validator {
+  static isNotNull(value: any, error: string): string | null {
+    return value !== null && value !== undefined ? null : error;
+  }
+
   static isValidEmail(email: string): boolean {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
