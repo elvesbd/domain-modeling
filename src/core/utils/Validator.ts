3d8532b3e11bc0cf7a4f8b43f6baadacbca7db63
elvesbd <elvesbd@yahoo.com>
2023-11-23 19:58:35 -0300

    feat: add validation for not empty

--- src/core/utils/Validator.ts
+++ src/core/utils/Validator.ts
@@ -3,6 +3,19 @@ export default class Validator {
     return value !== null && value !== undefined ? null : error;
   }
 
+  static isNotEmpty(value: string, error: string): string | null {
+    if (Validator.isNotNull(value, error)) return error;
+    return value.trim() !== '' ? null : error;
+  }
+
+  static isLessThan(
+    value: string | any[],
+    maxLength: number,
+    error: string
+  ): string | null {
+    return value.length < maxLength ? null : error;
+  }
+
   static isValidEmail(email: string): boolean {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
