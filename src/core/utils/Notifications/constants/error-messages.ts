c1bd4a959b15d5acbf03b534fc0f2335bd39e201
elvesbd <elvesbd@yahoo.com>
2023-11-26 19:29:48 -0300

    chore: add correct message

--- src/core/utils/Notifications/constants/error-messages.ts
+++ src/core/utils/Notifications/constants/error-messages.ts
@@ -2,7 +2,7 @@ export const errorMessages = {
   required: 'O campo é obrigatório.',
   empty: 'O campo não pode estar vazio.',
   maxLength: 'O comprimento máximo é {0} caracteres.',
-  minLength: 'O comprimento mínimo é de {0} caracteres',
+  minLength: 'O comprimento mínimo é {0} caracteres.',
   invalidEmail: 'O e-mail fornecido não é válido.',
   containsSpecialChars:
     'O campo não deve conter caracteres especiais.',
