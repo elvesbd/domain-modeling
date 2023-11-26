e1639f8f1eae4fbc4b23fcf6c946bbcdb479cba4
elvesbd <elvesbd@yahoo.com>
2023-11-26 20:05:57 -0300

    test: ensures throw error if name more than 120 characters

--- src/core/shared/value-objects/PersonName.ts
+++ src/core/shared/value-objects/PersonName.ts
@@ -7,7 +7,9 @@ export default class PersonName {
     this.name = name.trim();
 
     const errors = Notification.notifications(
-      Notification.isEmpty(this.name)
+      Notification.isEmpty(this.name),
+      Notification.isLessThan(this.name, 4),
+      Notification.isMoreThan(this.name, 120)
     );
 
     if (errors) throw new Error(errors.join(','));
--- test/core/shared/value-objects/PersonName.test.ts
+++ test/core/shared/value-objects/PersonName.test.ts
@@ -7,13 +7,26 @@ describe('Value Object - PersonName', () => {
     expect(createEmptyPersonName).toThrow(errorMessages.empty);
   });
 
-  it('should throw error if name less than four characters', () => {
-    const createShortPersonName = () => new PersonName('Joh');
+  it('should throw error if name less than 4 characters', () => {
+    const shortName = 'Joh';
+    const createShortPersonName = () => new PersonName(shortName);
     const expectedErrorMessage = errorMessages.minLength.replace(
       '{0}',
       '4'
     );
 
     expect(createShortPersonName).toThrow(expectedErrorMessage);
+  });
+
+  it('should throw error if name more than 120 characters', () => {
+    const longName =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+    const createLongPersonName = () => new PersonName(longName);
+    const expectedErrorMessage = errorMessages.maxLength.replace(
+      '{0}',
+      '120'
+    );
+
+    expect(createLongPersonName).toThrow(expectedErrorMessage);
   });
 });
