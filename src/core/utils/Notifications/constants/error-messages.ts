a4a2f268fffcb0e4c299099725d92768e9578bc2
elvesbd <elvesbd@yahoo.com>
2023-11-26 19:00:35 -0300

    test: ensures throw error if empty name

--- src/core/utils/Notifications/constants/error-messages.ts
+++ src/core/utils/Notifications/constants/error-messages.ts
@@ -2,5 +2,8 @@ export const errorMessages = {
   required: 'O campo é obrigatório.',
   empty: 'O campo não pode estar vazio.',
   maxLength: 'O comprimento máximo é {0} caracteres.',
+  minLength: 'O comprimento mínimo é de {0} caracteres',
   invalidEmail: 'O e-mail fornecido não é válido.',
+  containsSpecialChars:
+    'O campo não deve conter caracteres especiais.',
 };
