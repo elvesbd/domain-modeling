d686e1ee855c5f10efa1e1f9012f96abafe92b0b
elvesbd <elvesbd@yahoo.com>
2023-11-28 19:27:53 -0300

    test: ensures throw error if name contains special characters

--- test/core/shared/value-objects/PersonName.test.ts
+++ test/core/shared/value-objects/PersonName.test.ts
@@ -3,30 +3,38 @@ import { errorMessages } from '@/core/utils/Notifications/constants/error-messag
 
 describe('Value Object - PersonName', () => {
   it('should throw error if empty name', () => {
-    const createEmptyPersonName = () => new PersonName('');
-    expect(createEmptyPersonName).toThrow(errorMessages.empty);
+    const execute = () => new PersonName('');
+    expect(execute).toThrow(errorMessages.empty);
   });
 
   it('should throw error if name less than 4 characters', () => {
     const shortName = 'Joh';
-    const createShortPersonName = () => new PersonName(shortName);
+    const execute = () => new PersonName(shortName);
     const expectedErrorMessage = errorMessages.minLength.replace(
       '{0}',
       '4'
     );
 
-    expect(createShortPersonName).toThrow(expectedErrorMessage);
+    expect(execute).toThrow(expectedErrorMessage);
   });
 
   it('should throw error if name more than 120 characters', () => {
     const longName =
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
-    const createLongPersonName = () => new PersonName(longName);
+    const execute = () => new PersonName(longName);
     const expectedErrorMessage = errorMessages.maxLength.replace(
       '{0}',
       '120'
     );
 
-    expect(createLongPersonName).toThrow(expectedErrorMessage);
+    expect(execute).toThrow(expectedErrorMessage);
+  });
+
+  it('should throw error if name contains special characters', () => {
+    const name = '@invalid name';
+    const execute = () => new PersonName(name);
+    const expectedErrorMessage = errorMessages.containsOnlyLetters;
+
+    expect(execute).toThrow(expectedErrorMessage);
   });
 });
