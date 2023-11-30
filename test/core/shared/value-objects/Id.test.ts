146758aa0d0713e645ebd204ccbffc7f59add8e7
elvesbd <elvesbd@yahoo.com>
2023-11-30 20:00:33 -0300

    test: ensures create valid id

--- test/core/shared/value-objects/Id.test.ts
+++ test/core/shared/value-objects/Id.test.ts
@@ -5,4 +5,10 @@ describe('Value Object - Id', () => {
     const execute = () => new Id('123');
     expect(execute).toThrow(new Error('Invalid id!'));
   });
+
+  it('should create valid id', () => {
+    const id = new Id();
+    expect(id.value).toBeDefined();
+    expect(id.value).toHaveLength(36);
+  });
 });
