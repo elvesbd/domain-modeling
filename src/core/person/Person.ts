d65f116e18326bded860adaef6960f829cb8a0ec
elvesbd <elvesbd@yahoo.com>
2023-12-03 19:35:01 -0300

    chore: add value object cpf in person domain

--- src/core/person/Person.ts
+++ src/core/person/Person.ts
@@ -1,17 +1,21 @@
 import Id from '../shared/value-objects/Id';
+import Cpf from '../shared/value-objects/Cpf';
 import PersonName from '../shared/value-objects/PersonName';
 
 export type PersonNameProps = {
   id?: string;
+  cpf: string;
   name: string;
 };
 
 export default class Person {
   readonly id: Id;
+  readonly cpf: Cpf;
   readonly name: PersonName;
 
   constructor(props: PersonNameProps) {
     this.id = new Id(props.id);
+    this.cpf = new Cpf(props.cpf);
     this.name = new PersonName(props.name);
   }
 }
--- test/core/person/Person.test.ts
+++ test/core/person/Person.test.ts
@@ -4,9 +4,11 @@ describe('Domain - Person', () => {
   it('should create name on success', () => {
     const props: PersonNameProps = {
       name: 'Elvis Presley',
+      cpf: '280.012.389-38',
     };
     const person = new Person(props);
     expect(person.id.value).toBeDefined();
     expect(person.name.fullName).toBe('Elvis Presley');
+    expect(person.cpf.value).toBe('280.012.389-38');
   });
 });
