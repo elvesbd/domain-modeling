7c5dd533dab605c26a6177be5f0f98c1beccb4f8
elvesbd <elvesbd@yahoo.com>
2023-12-05 19:41:02 -0300

    test: ensures that methods for compare ids is correct

--- test/core/shared/value-objects/Id.test.ts
+++ test/core/shared/value-objects/Id.test.ts
@@ -6,7 +6,24 @@ describe('Value Object - Id', () => {
     expect(execute).toThrow(new Error('Invalid id!'));
   });
 
-  it('should create valid id', () => {
+  it('should be compare if id is equals', () => {
+    const id = new Id();
+    const otherId = new Id(id.value);
+    expect(id.isEquals(otherId)).toBeTruthy();
+  });
+
+  it('should be compare if id is different', () => {
+    const id = new Id();
+    const otherId = new Id(id.value);
+    expect(id.isDifferent(otherId)).toBeFalsy();
+  });
+
+  it('should be compare if otherId is undefined', () => {
+    const id = new Id();
+    expect(id.isEquals(undefined as any)).toBeFalsy();
+  });
+
+  it('should be create valid id', () => {
     const id = new Id();
     expect(id.value).toBeDefined();
     expect(id.value).toHaveLength(36);
