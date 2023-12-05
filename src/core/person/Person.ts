eb3e4b515c44fc67f16603a4c1f4ff469df3e26e
elvesbd <elvesbd@yahoo.com>
2023-12-05 19:21:13 -0300

    feat: created clone method

--- src/core/person/Person.ts
+++ src/core/person/Person.ts
@@ -2,7 +2,7 @@ import Id from '../shared/value-objects/Id';
 import Cpf from '../shared/value-objects/Cpf';
 import PersonName from '../shared/value-objects/PersonName';
 
-export type PersonNameProps = {
+export type PersonProps = {
   id?: string;
   cpf: string;
   name: string;
@@ -12,10 +12,16 @@ export default class Person {
   readonly id: Id;
   readonly cpf: Cpf;
   readonly name: PersonName;
+  readonly props: PersonProps;
 
-  constructor(props: PersonNameProps) {
+  constructor(props: PersonProps) {
     this.id = new Id(props.id);
     this.cpf = new Cpf(props.cpf);
     this.name = new PersonName(props.name);
+    this.props = { ...props, id: this.id.value };
+  }
+
+  clone(props: Partial<PersonProps>) {
+    return new Person({ ...this.props, ...props });
   }
 }
