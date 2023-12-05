9cb6a3c96c79e1d6102c868617e16bbfb6dfe295
elvesbd <elvesbd@yahoo.com>
2023-12-05 19:22:20 -0300

    test: ensures that it clones the object with the updated name

--- test/core/person/Person.test.ts
+++ test/core/person/Person.test.ts
@@ -1,14 +1,25 @@
-import Person, { PersonNameProps } from '@/core/person/Person';
+import Person, { PersonProps } from '@/core/person/Person';
 
 describe('Domain - Person', () => {
+  const props: PersonProps = {
+    name: 'Elvis Presley',
+    cpf: '280.012.389-38',
+  };
+
   it('should create name on success', () => {
-    const props: PersonNameProps = {
-      name: 'Elvis Presley',
-      cpf: '280.012.389-38',
-    };
     const person = new Person(props);
     expect(person.id.value).toBeDefined();
     expect(person.name.fullName).toBe('Elvis Presley');
     expect(person.cpf.value).toBe('280.012.389-38');
+  });
+
+  it('should be clone object with name updated', () => {
+    const person = new Person(props);
+    const newPerson = person.clone({ name: 'Elvis Brito' });
+
+    expect(newPerson.id.value).toBe(person.id.value);
+    expect(newPerson.cpf.value).toBe(person.cpf.value);
+    expect(newPerson.name.fullName).not.toBe(person.name.fullName);
+    expect(newPerson.name.fullName).toBe('Elvis Brito');
   });
 });
