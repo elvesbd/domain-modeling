529c5673af1f46312806380f1a96b404073cf068
elvesbd <elvesbd@yahoo.com>
2023-12-13 16:05:22 -0300

    feat: create test data builder

--- test/core/person/Person.test.ts
+++ test/core/person/Person.test.ts
@@ -1,25 +1,21 @@
-import Person, { PersonProps } from '@/core/person/Person';
+import PersonBuilder from '@/test/data-builder/person/PersonBuilder';
 
 describe('Domain - Person', () => {
-  const props: PersonProps = {
-    name: 'Elvis Presley',
-    cpf: '280.012.389-38',
-  };
-
   it('should create name on success', () => {
-    const person = new Person(props);
+    const person = PersonBuilder.create().build();
+
     expect(person.id.value).toBeDefined();
-    expect(person.name.fullName).toBe('Elvis Presley');
-    expect(person.cpf.value).toBe('280.012.389-38');
+    expect(person.cpf.value).toBe(person.cpf.value);
+    expect(person.name.fullName).toBe(person.name.fullName);
   });
 
   it('should be clone object with name updated', () => {
-    const person = new Person(props);
+    const person = PersonBuilder.create().build();
     const newPerson = person.clone({ name: 'Elvis Brito' });
 
     expect(newPerson.id.value).toBe(person.id.value);
     expect(newPerson.cpf.value).toBe(person.cpf.value);
-    expect(newPerson.name.fullName).not.toBe(person.name.fullName);
     expect(newPerson.name.fullName).toBe('Elvis Brito');
+    expect(newPerson.name.fullName).not.toBe(person.name.fullName);
   });
 });
