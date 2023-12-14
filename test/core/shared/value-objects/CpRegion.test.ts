f8bbc75f8ca2d1e2ff69b8fd3107ec92a55ca352
elvesbd <elvesbd@yahoo.com>
2023-12-14 19:15:03 -0300

    test: ensures that returns the DF region to region code 1

--- test/core/shared/value-objects/CpRegion.test.ts
+++ test/core/shared/value-objects/CpRegion.test.ts
@@ -6,13 +6,21 @@ describe('Value Object - CpfRegion', () => {
     const region = CpfRegion.getByCode(regionCode);
 
     expect(region.code).toBe(regionCode);
-    expect(region.states[0]).toBe('RS');
+    expect(region.states).toContain('RS');
   });
 
   it('ensures that returns the RS region if the cpf entered belongs to region 0', () => {
     const cpf = '345.799.510-93';
     const region = CpfRegion.getByCpf(cpf);
 
-    expect(region.states[0]).toBe('RS');
+    expect(region.states).toContain('RS');
+  });
+
+  it('ensures that returns the DF region to region code 1', () => {
+    const regionCode = 1;
+    const region = CpfRegion.getByCode(regionCode);
+
+    expect(region.code).toBe(regionCode);
+    expect(region.states).toContain('DF');
   });
 });
