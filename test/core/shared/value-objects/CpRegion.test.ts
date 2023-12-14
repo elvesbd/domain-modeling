0b605c8bd6bcb1c216765d98b33f6323faacb25c
elvesbd <elvesbd@yahoo.com>
2023-12-14 18:54:21 -0300

    test: ensures that returns the RS region if the cpf entered belongs to region 0

--- test/core/shared/value-objects/CpRegion.test.ts
+++ test/core/shared/value-objects/CpRegion.test.ts
@@ -1,11 +1,18 @@
 import CpfRegion from '@/core/shared/value-objects/CpfRegion';
 
 describe('Value Object - CpfRegion', () => {
-  it('ensures that it returns the RS region to region code 0', () => {
+  it('ensures that returns the RS region to region code 0', () => {
     const regionCode = 0;
     const region = CpfRegion.getByCode(regionCode);
 
     expect(region.code).toBe(regionCode);
+    expect(region.states[0]).toBe('RS');
+  });
+
+  it('ensures that returns the RS region if the cpf entered belongs to region 0', () => {
+    const cpf = '345.799.510-93';
+    const region = CpfRegion.getByCpf(cpf);
+
     expect(region.states[0]).toBe('RS');
   });
 });
