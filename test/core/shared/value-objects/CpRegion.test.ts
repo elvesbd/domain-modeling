481a9bb57b0aa35ccacc27db57a895dcdb3d37f6
elvesbd <elvesbd@yahoo.com>
2023-12-14 19:19:59 -0300

    test: ensures that returns the MS region to region code 1

--- test/core/shared/value-objects/CpRegion.test.ts
+++ test/core/shared/value-objects/CpRegion.test.ts
@@ -45,4 +45,12 @@ describe('Value Object - CpfRegion', () => {
 
     expect(region.states).toContain('GO');
   });
+
+  it('ensures that returns the MS region to region code 1', () => {
+    const regionCode = 1;
+    const region = CpfRegion.getByCode(regionCode);
+
+    expect(region.code).toBe(regionCode);
+    expect(region.states).toContain('MS');
+  });
 });
