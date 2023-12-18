bb8a6a321d59971fc5748f5cf3bba4802f22e0d8
elvesbd <elvesbd@yahoo.com>
2023-12-18 19:59:04 -0300

    test: ensures that returns the RN region if the cpf entered belongs to region 4

--- test/core/shared/value-objects/CpRegion.test.ts
+++ test/core/shared/value-objects/CpRegion.test.ts
@@ -278,4 +278,11 @@ describe('Value Object - CpfRegion', () => {
     expect(region.code).toBe(regionCode);
     expect(region.states).toContain('RN');
   });
+
+  it('ensures that returns the RN region if the cpf entered belongs to region 4', () => {
+    const cpf = '345.799.514-93';
+    const region = CpfRegion.getByCpf(cpf);
+
+    expect(region.states).toContain('RN');
+  });
 });
